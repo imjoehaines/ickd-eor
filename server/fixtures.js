ba59a8bbe8044446b452b68644fa26d13209b6eb
Joe Haines <hello@joehaines.co.uk>
2015-04-03 13:28:18 +0100

    Updated styles & started work on todo items

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,7 +1,31 @@
 if (Todo.find().count() === 0) {
     Todo.insert({
-        task: 'finish this',
+        task: 'Do something',
         createdAt: new Date(),
-        status: 'In progress'
+        done: false
+    });
+
+    Todo.insert({
+        task: 'Ajskj kjasdk',
+        createdAt: new Date(),
+        done: false
+    });
+
+    Todo.insert({
+        task: 'Alsdjf dshfljks',
+        createdAt: new Date(),
+        done: false
+    });
+
+    Todo.insert({
+        task: 'Oaskhas uyasiuh',
+        createdAt: new Date(),
+        done: false
+    });
+
+    Todo.insert({
+        task: 'Oaskjhas iusadj',
+        createdAt: new Date(),
+        done: false
     });
 }
