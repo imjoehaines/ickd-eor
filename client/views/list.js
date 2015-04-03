fdc061008feeb814b318ea4085b6aabb8141ccb8
Joe Haines <hello@joehaines.co.uk>
2015-04-03 16:09:19 +0100

    Add new todo items

--- client/views/list.js
+++ client/views/list.js
@@ -1,5 +1,21 @@
 Template.list.helpers({
     todoList: function () {
-        return Todo.find();
+        return Todo.find({}, {sort: {createdAt: -1}});
+    }
+});
+
+Template.list.events({
+    'submit .addTodo': function(event) {
+        event.preventDefault();
+
+        var $newTodo = $('.newTodo');
+
+        Todo.insert({
+          task: $newTodo.val(),
+          createdAt: new Date(),
+          done: false
+        });
+
+        $newTodo.val('');
     }
 });
